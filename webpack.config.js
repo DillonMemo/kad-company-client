a6329cbe60a0a7a5ba313fb6cd3e70b4d254d99a
Dillonmemo <arta1069@gmail.com>
2020-07-16 15:18:30 +0900

    clear

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-module.export = (env) => {
-  console.log("dotenv :", process.env.NODE_ENV, env);
-  return require(`./webpack.${env}.js`);
-};
