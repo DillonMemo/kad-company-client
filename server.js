2e913f09d6f6c99fe34397a9014c1340415eab41
Dillonmemo <arta1069@gmail.com>
2020-07-29 17:06:12 +0900

    set letencrypt

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
+  console.log("dirname :", express.static(__dirname));
+  // server.use(express.static(__dirname), { dotfiles: "allow" });
+
+  server.get(
+    "/.well-known/acme-challenge/4o6Vu2hlv3paSsA3fwSKwz4bUr4tJy5VYlnQ9jBMunw",
+    (req, res) => {
+      res
+        .status(200)
+        .set("content-type", "text/plain")
+        .send(
+          "4o6Vu2hlv3paSsA3fwSKwz4bUr4tJy5VYlnQ9jBMunw.Fh5k9QYsWVY0V0N4E3XQtX_szVvx25QHhCQbAQKKLHE"
+        );
+    }
+  );
+
   server.all("*", (req, res) => {
     return handle(req, res);
   });
